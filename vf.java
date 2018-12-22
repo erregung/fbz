851de49e954705f24cf5ca920b79e038b825d543
erregung <45234021+erregung@users.noreply.github.com>
2018-12-22 11:37:39 +0200

    Update vf.java

--- vf.java
+++ vf.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class LaskuLaskuri {
   public static void main(String[] args) {
-    Scanner lukija = new Scanner(system.in);
+    Scanner lukija = new Scanner(System.in);
     
     System.out.println("Maksajien määrä:");
     int maksajat = Integer.valueOf(lukija.nextLine());
