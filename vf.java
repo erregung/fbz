047ddeb13097119380f83de0170770cfd0ea8f49
erregung <45234021+erregung@users.noreply.github.com>
2018-12-22 11:31:39 +0200

    Create vf.java

---
+++ vf.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class LaskuLaskuri {
+  public static void main(String[] args) {
+    Scanner lukija = new Scanner(system.in);
+    
+    System.out.println("Maksajien määrä:");
+    int maksajat = Integer.valueOf(lukija.nextLine());
+    System.out.println("Loppusumma:");
+    Double summa = Double.valueOf(lukija.nextLine());
+    
+    if (maksajat > 0 && summa > 0) {
+      System.out.println("Kukin maksaa " + (summa / maksajat) + " €.");
+    } else {
+      System.out.println("Hämmennys.");
+    }
+  }
+}
+
