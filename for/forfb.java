0ce1ab05c16ef621d5e671c9506798a53445ecee
erregung <kingelin@protonmail.com>
2018-12-30 11:05:03 +0200

    Rikottu koodia lisää, en osaa käyttää foria vielä

--- for/forfb.java
+++ for/forfb.java
@@ -1,5 +1,23 @@
+//koko paska rikki anyway
 public class forfb {
 	public static void main(String[] args) {
-		for(
+		for(int j = 0; j < 100; j++);
+		/*ilmeisesti for-loopin sisäiset muuttujat eivät toimi sen 
+		ulkopuolella? */
+			int i = 0;
+			if (i % 3 == 0 && i % 5 == 0) {
+				System.out.println("FizzBuzz");
+				i++;
+			}
+			if (i % 3 == 0) {
+				System.out.println("Fizz");
+				i++;
+			}
+			if (i % 5 == 0) {
+				System.out.println("Buzz");
+				i++;
+			}
+			System.out.println(i);
+			i++;
 	}
 }	
