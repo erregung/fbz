f1eabe74261bfbfb27c8b3f087335fd8da509ebe
erregung <kingelin@protonmail.com>
2019-01-18 15:50:09 +0200

    Jotain roskaa

--- for/forfb.java
+++ for/forfb.java
@@ -1,9 +1,6 @@
-//koko paska rikki anyway
 public class forfb {
 	public static void main(String[] args) {
 		for(int i = 0; i < 100; i++);
-		/*ilmeisesti for-loopin sisäiset muuttujat eivät toimi sen 
-		ulkopuolella? */
 			if (i % 3 == 0 && i % 5 == 0) {
 				System.out.println("FizzBuzz");
 				
